1e387f6f237e35b59d8c348f9836099faf486e9d
Bruno Ricardo Secco <brunoricardosecco@hotmail.com>
2020-10-27 22:05:17 -0300

    fix: ajust baseUrl api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://rest-countries-api.herokuapp.com',
   headers: { accept: 'application/json', 'content-type': 'application/json' },
 });
 
